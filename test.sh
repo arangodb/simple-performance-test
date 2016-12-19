8e7166e98f521f3852112fdd18893f28d1d56b92
Andreas Streichardt <andreas@arangodb.com>
2016-12-19 18:35:57 +0100

    remove time

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ done
 set -e
 
 SEQWRITEREADSTART=$(date +%s%N | cut -b1-13)
-time build/bin/arangosh --server.endpoint tcp://127.0.0.1:1234 --javascript.execute-string "col = db._create('testung');for (i=0;i<100000;i++) col.insert({_key: 'key' + i, 'test': 'testung'});for (i=0;i<100000;i++) col.document('key' + i);"
+build/bin/arangosh --server.endpoint tcp://127.0.0.1:1234 --javascript.execute-string "col = db._create('testung');for (i=0;i<100000;i++) col.insert({_key: 'key' + i, 'test': 'testung'});for (i=0;i<100000;i++) col.document('key' + i);"
 SEQWRITEREADEND=$(date +%s%N | cut -b1-13)
 kill $PID
 wait
