412c8d514f67986f7c3bda0593d5f4e4e52c9d37
Frank Celler <frank@arangodb.com>
2019-06-20 14:22:21 +0200

    edges only

---
+++ run-small-edges.js
@@ -0,0 +1,13 @@
+function main () {
+  require('./test').test({
+    outputCsv: true,
+    small: true,
+
+    documents: false,
+    edges: true,
+    search: false,
+    phrase: false,
+    crud: false,
+    crudSearch: false
+  });
+}
