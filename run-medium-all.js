fa62c58b6139bbba9a6f46136885d3a34a262260
Frank Celler <frank@arangodb.com>
2020-06-13 08:42:10 +0200

    run-medium-all config

---
+++ run-medium-all.js
@@ -0,0 +1,18 @@
+function main () {
+  require("./test").test({
+    outputCsv: true,
+    medium: true,
+
+    documents: true,
+    edges: true,
+    search: true,
+    phrase: true,
+    noMaterializationSearch: true,
+    crud: true,
+    crudSearch: true,
+    subqueryTests: true
+  });
+}
+if (typeof arango !== undefined) {
+  main();
+}
